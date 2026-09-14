89335dc787fb18eee91e0a518898e32d4e2b3b5c
tony-py25 <kariukikamunya@gmail.com>
2026-09-14 19:52:11 +0300

    Add basic SemaAI interaction

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".button");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      console.log("SemaAI MVP button clicked");
+    });
+  });
+});
